75253d985c8e2f84204216fa88a22aa48abaa71e
Crisrioja <cristiantorresrioja@gmail.com>
2023-10-24 15:38:55 -0600

    funciones

---
+++ funciones.py
@@ -0,0 +1,21 @@
+#===========================================================
+# Cristian Torres Rioja
+#===========================================================
+#   Paradigmas de programación
+#   Matemática Algoritmica
+#   ESFM-IPN    oct_2023
+#===========================================================
+
+#===========================================================
+#   Fuciones suma, promedio y potencia
+#===========================================================
+
+def suma(x,y):
+    return x+y
+
+def promedio(x,y):
+    return 0.5*(x+y)
+
+def potencia(x,y):
+    return x**y
+
