bc2eb67d22286273fdcca2287ef6a20cdc0c3b04
Crisrioja <cristiantorresrioja@gmail.com>
2023-10-24 15:36:16 -0600

    12_AtraparErrores

---
+++ 12_AtraparErrores.py
@@ -0,0 +1,53 @@
+#===========================================================
+# Cristian Torres Rioja
+#===========================================================
+#   Paradigmas de programación
+#   Matemática Algoritmica
+#   ESFM-IPN    oct_2023
+#===========================================================
+
+#===========================================================
+#   La clase ClienteBancario esté en el subdirectorio
+#   aplicacion/banco/
+#===========================================================
+
+from aplicacion.banco.cliente_bancario import ClienteBancario
+
+#===========================================================
+#   try: intenta (correr las instrucciones)
+#   except:  atrapar el error en una variable e
+#   e se puede convertir a string
+#===========================================================
+
+#===========================================================
+#   Error por sacar más dinero del que tiene
+#===========================================================
+try:
+    cliente = ClienteBancario("Jaime Andrade", "Hernández Sánchez", 28, 0.0)
+    cliente.guardarDinero(300)
+    print(cliente.imprimirInfo())
+    cliente.retirarDinero(400)
+    print(cliente.imprimirInfo())
+
+#===========================================================
+#   Exception es el objeto más general de error
+#===========================================================
+
+except Exception as e:
+    print("Error: "+ str(e))
+
+#===========================================================
+#   Error por usar un atributo privado
+#===========================================================
+
+try:
+    print(cliente.__nombres)
+except Exception as ex:
+    print("Error: " + str(ex))
+
+#===========================================================
+#   Forma correcta
+#===========================================================
+
+print(cliente.nombres)
+
