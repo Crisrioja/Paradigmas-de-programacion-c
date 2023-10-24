9fdde4b3f2dab0a10f0d43d0be43e0aa83895d49
Crisrioja <cristiantorresrioja@gmail.com>
2023-10-24 15:38:17 -0600

    13_Utilizarfunciones

---
+++ 13_Utilizarfunciones.py
@@ -0,0 +1,17 @@
+#===========================================================
+# Cristian Torres Rioja
+#===========================================================
+#   Paradigmas de programación
+#   Matemática Algoritmica
+#   ESFM-IPN    oct_2023
+#===========================================================
+
+#===========================================================
+#   De funciones.py traer las funciones suma y promedio
+#===========================================================
+
+from funciones import suma,promedio
+
+print(suma(100,200))
+print(promedio(100,200))
+
